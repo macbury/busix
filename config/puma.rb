cdc2991c24ff3ea6f8ab526e88538e9af2b5ee72
Arkadiusz Buras <me@macbury.ninja>
2020-01-12 16:23:40 +0000

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
